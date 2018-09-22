a93139f451d764837b4668f31266eddd30e5d092
MarkHoward <markjameshoward@hotmail.co.uk>
2018-09-23 07:46:55 +1200

    initial commit

---
+++ main.ts
@@ -0,0 +1,10 @@
+import { LitElement, html, customElement } from "@polymer/lit-element";
+
+@customElement("my-app")
+class MyApp extends LitElement {
+  render() {
+    return html`
+    <h3>Test</h3>
+    `;
+  }
+}
